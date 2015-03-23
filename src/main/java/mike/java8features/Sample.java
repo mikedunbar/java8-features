69a3e35c7f994bf111b1ffc9ed9685acaad6a803
Mike Dunbar <miketdunbar@gmail.com>
2015-03-23 09:57:48 -0400

    formatting

--- src/main/java/mike/java8features/Sample.java
+++ src/main/java/mike/java8features/Sample.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Sample
-{
+public class Sample {
+
     public static void main( String[] args ) {
         // Filter a list using the new stream and lamba features
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
